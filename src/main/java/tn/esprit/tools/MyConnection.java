ecea77b19a45f8b56387f0673a670f02997aeb57
elyesexe <chaouch.elyes@esprit.tn>
2026-04-04 22:23:09 +0100

    Restore database name to sport_insight

--- src/main/java/tn/esprit/tools/MyConnection.java
+++ src/main/java/tn/esprit/tools/MyConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/sport_insight-java?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/sport_insight?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
